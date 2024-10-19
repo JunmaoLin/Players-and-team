fbf3c7ce3ecc4201f8b67bdf617ae970ca72df99
JunmaoLin <junmaol02@gmail.com>
2024-10-19 03:11:15 -0400

    Create FullTeamException.java

---
+++ FullTeamException.java
@@ -0,0 +1,9 @@
+/**
+ * This class represents the FullTeamException
+*/
+
+public class FullTeamException extends Exception{
+    public FullTeamException(String errorMessage) {
+        super(errorMessage);
+    }
+}
